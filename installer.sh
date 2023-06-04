e50b49204c758ab167a715f30369641c1194e939
marked9999 <jewelsytofficial@gmail.com>
2023-06-04 18:44:16 +0000

    try

--- installer.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/js-emulators/desmond/main/dist/desmond.min.js
-wget https://raw.githubusercontent.com/js-emulators/desmond/main/dist/desmond.wasm
-
-npm install http-server -g
